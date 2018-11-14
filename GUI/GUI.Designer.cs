42aaa371ee60549b5fa1a54b089c1f8d2f3b68f8
Kasper Grosbøll Arknæs <kasp9727@edu.eal.dk>
2018-11-14 10:28:00 +0100

    Add GUI class Add DragControl Class

---
+++ GUI/GUI.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace GUI
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.HelpBtn = new System.Windows.Forms.Button();
+            this.ClientBtn = new System.Windows.Forms.Button();
+            this.EmployeeBtn = new System.Windows.Forms.Button();
+            this.CaseBtn = new System.Windows.Forms.Button();
+            this.MenuPictureBtn = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.CloseWindowBtn = new System.Windows.Forms.PictureBox();
+            this.MenuSlideTimer = new System.Windows.Forms.Timer(this.components);
+            this.dragControl1 = new DragControl();
+            this.MenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuPictureBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseWindowBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.MenuPanel.Controls.Add(this.HelpBtn);
+            this.MenuPanel.Controls.Add(this.ClientBtn);
+            this.MenuPanel.Controls.Add(this.EmployeeBtn);
+            this.MenuPanel.Controls.Add(this.CaseBtn);
+            this.MenuPanel.Controls.Add(this.MenuPictureBtn);
+            this.MenuPanel.Controls.Add(this.pictureBox1);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 37);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(69, 566);
+            this.MenuPanel.TabIndex = 1;
+            // 
+            // HelpBtn
+            // 
+            this.HelpBtn.FlatAppearance.BorderSize = 0;
+            this.HelpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HelpBtn.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.HelpBtn.Image = ((System.Drawing.Image)(resources.GetObject("HelpBtn.Image")));
+            this.HelpBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HelpBtn.Location = new System.Drawing.Point(0, 496);
+            this.HelpBtn.Name = "HelpBtn";
+            this.HelpBtn.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.HelpBtn.Size = new System.Drawing.Size(347, 68);
+            this.HelpBtn.TabIndex = 4;
+            this.HelpBtn.Text = "        Hjælp";
+            this.HelpBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.HelpBtn.UseVisualStyleBackColor = true;
+            // 
+            // ClientBtn
+            // 
+            this.ClientBtn.FlatAppearance.BorderSize = 0;
+            this.ClientBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClientBtn.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ClientBtn.Image = ((System.Drawing.Image)(resources.GetObject("ClientBtn.Image")));
+            this.ClientBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ClientBtn.Location = new System.Drawing.Point(0, 281);
+            this.ClientBtn.Name = "ClientBtn";
+            this.ClientBtn.Padding = new System.Windows.Forms.Padding(7, 0, 0, 0);
+            this.ClientBtn.Size = new System.Drawing.Size(347, 68);
+            this.ClientBtn.TabIndex = 4;
+            this.ClientBtn.Text = "     Klienter";
+            this.ClientBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ClientBtn.UseVisualStyleBackColor = true;
+            this.ClientBtn.Click += new System.EventHandler(this.ClientBtn_Click);
+            this.ClientBtn.MouseEnter += new System.EventHandler(this.ClientBtn_MouseEnter);
+            this.ClientBtn.MouseLeave += new System.EventHandler(this.ClientBtn_MouseLeave);
+            // 
+            // EmployeeBtn
+            // 
+            this.EmployeeBtn.FlatAppearance.BorderSize = 0;
+            this.EmployeeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EmployeeBtn.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.EmployeeBtn.Image = ((System.Drawing.Image)(resources.GetObject("EmployeeBtn.Image")));
+            this.EmployeeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.EmployeeBtn.Location = new System.Drawing.Point(0, 218);
+            this.EmployeeBtn.Name = "EmployeeBtn";
+            this.EmployeeBtn.Padding = new System.Windows.Forms.Padding(7, 0, 0, 0);
+            this.EmployeeBtn.Size = new System.Drawing.Size(347, 68);
+            this.EmployeeBtn.TabIndex = 4;
+            this.EmployeeBtn.Text = "     Ansatte";
+            this.EmployeeBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.EmployeeBtn.UseVisualStyleBackColor = true;
+            this.EmployeeBtn.Click += new System.EventHandler(this.EmployeeBtn_Click);
+            this.EmployeeBtn.MouseEnter += new System.EventHandler(this.EmployeeBtn_MouseEnter);
+            this.EmployeeBtn.MouseLeave += new System.EventHandler(this.EmployeeBtn_MouseLeave);
+            // 
+            // CaseBtn
+            // 
+            this.CaseBtn.FlatAppearance.BorderSize = 0;
+            this.CaseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CaseBtn.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CaseBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CaseBtn.Image = ((System.Drawing.Image)(resources.GetObject("CaseBtn.Image")));
+            this.CaseBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CaseBtn.Location = new System.Drawing.Point(0, 155);
+            this.CaseBtn.Name = "CaseBtn";
+            this.CaseBtn.Padding = new System.Windows.Forms.Padding(7, 0, 0, 0);
+            this.CaseBtn.Size = new System.Drawing.Size(347, 68);
+            this.CaseBtn.TabIndex = 4;
+            this.CaseBtn.Text = "      Sager";
+            this.CaseBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.CaseBtn.UseVisualStyleBackColor = true;
+            this.CaseBtn.Click += new System.EventHandler(this.CaseBtn_Click);
+            this.CaseBtn.MouseEnter += new System.EventHandler(this.CaseBtn_MouseEnter);
+            this.CaseBtn.MouseLeave += new System.EventHandler(this.CaseBtn_MouseLeave);
+            // 
+            // MenuPictureBtn
+            // 
+            this.MenuPictureBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuPictureBtn.Image = ((System.Drawing.Image)(resources.GetObject("MenuPictureBtn.Image")));
+            this.MenuPictureBtn.Location = new System.Drawing.Point(0, 32);
+            this.MenuPictureBtn.Name = "MenuPictureBtn";
+            this.MenuPictureBtn.Size = new System.Drawing.Size(69, 69);
+            this.MenuPictureBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MenuPictureBtn.TabIndex = 3;
+            this.MenuPictureBtn.TabStop = false;
+            this.MenuPictureBtn.Click += new System.EventHandler(this.MenuPictureBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.TopPanel.Controls.Add(this.CloseWindowBtn);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1226, 37);
+            this.TopPanel.TabIndex = 2;
+            // 
+            // CloseWindowBtn
+            // 
+            this.CloseWindowBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseWindowBtn.Image = ((System.Drawing.Image)(resources.GetObject("CloseWindowBtn.Image")));
+            this.CloseWindowBtn.Location = new System.Drawing.Point(1192, 6);
+            this.CloseWindowBtn.Name = "CloseWindowBtn";
+            this.CloseWindowBtn.Size = new System.Drawing.Size(25, 24);
+            this.CloseWindowBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CloseWindowBtn.TabIndex = 3;
+            this.CloseWindowBtn.TabStop = false;
+            this.CloseWindowBtn.Click += new System.EventHandler(this.CloseWindowBtn_Click);
+            // 
+            // MenuSlideTimer
+            // 
+            this.MenuSlideTimer.Interval = 10;
+            this.MenuSlideTimer.Tag = "";
+            this.MenuSlideTimer.Tick += new System.EventHandler(this.MenuSlideTimer_Tick);
+            // 
+            // dragControl1
+            // 
+            this.dragControl1.SelectControl = this.TopPanel;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1226, 603);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GUI";
+            this.Text = "GUI";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.MenuPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MenuPictureBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CloseWindowBtn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Button HelpBtn;
+        private System.Windows.Forms.Button ClientBtn;
+        private System.Windows.Forms.Button EmployeeBtn;
+        private System.Windows.Forms.Button CaseBtn;
+        private System.Windows.Forms.PictureBox MenuPictureBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.PictureBox CloseWindowBtn;
+        private System.Windows.Forms.Timer MenuSlideTimer;
+        private DragControl dragControl1;
+    }
+}
---
+++ GUI/GUI.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    // OBS !!!  Alle kommentare refererer til nedenstående kode.
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+            panelWidth = MenuPanel.Width;
+            hidden = true;
+        }
+        //Sørger for at det første der bliver loaded, når Formen loader, er Cases
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            CaseBtn.PerformClick();
+        }
+        //Står for hide/show toggle på menuen
+        private int panelWidth;
+        private bool hidden;
+        private void MenuPictureBtn_Click(object sender, EventArgs e)
+        {
+            if (hidden)
+                MenuSlideTimer.Start();
+            else
+                MenuSlideTimer.Start();
+        }
+        private void MenuSlideTimer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                MenuPanel.Width = MenuPanel.Width + 20;
+                if (MenuPanel.Width >= 310)
+                {
+                    MenuSlideTimer.Stop();
+                    hidden = false;
+                    this.Refresh();
+                }
+            }
+            else
+            {
+                MenuPanel.Width = MenuPanel.Width - 20;
+                if (MenuPanel.Width <= panelWidth)
+                {
+                    MenuSlideTimer.Stop();
+                    hidden = true;
+                    this.Refresh();
+                }
+            }
+        }
+        //Står for at lukke programmet ned når man trykker på knappen
+        private void CloseWindowBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //Står for at ændre tekstfarven når musen hover på knappen
+        private void buttonForeColorHover(Button button)
+        {
+            button.ForeColor = Color.SteelBlue;
+        }
+        //Står for at ændre tekstfarven tilbage når musen forlader knappen
+        private void buttonForeColorLeave(Button button)
+        {
+            button.ForeColor = Color.Black;
+        }
+        //Står for at highlighte den sidste knap der er blevet trykket på samt at resette farven når en ny knap trykkes på. 
+        private Button _lastButtonClicked;
+        private void buttonBackColor(Button button)
+        {
+            if (_lastButtonClicked != null)
+                _lastButtonClicked.BackColor = Color.Transparent;
+
+            _lastButtonClicked = button as Button;
+            _lastButtonClicked.BackColor = Color.SteelBlue;
+        }
+        //Events for CaseBtn
+        private void CaseBtn_MouseEnter(object sender, EventArgs e)
+        {
+            buttonForeColorHover(CaseBtn);
+        }
+        private void CaseBtn_MouseLeave(object sender, EventArgs e)
+        {
+            buttonForeColorLeave(CaseBtn);
+        }
+        private void CaseBtn_Click(object sender, EventArgs e)
+        {
+            buttonBackColor(CaseBtn);
+        }
+        //Events for EmployeeBtn
+        private void EmployeeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            buttonForeColorHover(EmployeeBtn);
+        }
+        private void EmployeeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            buttonForeColorLeave(EmployeeBtn);
+        }
+        private void EmployeeBtn_Click(object sender, EventArgs e)
+        {
+            buttonBackColor(EmployeeBtn);
+        }
+        //Events for ClientBtn
+        private void ClientBtn_MouseEnter(object sender, EventArgs e)
+        {
+            buttonForeColorHover(ClientBtn);
+        }
+        private void ClientBtn_MouseLeave(object sender, EventArgs e)
+        {
+            buttonForeColorLeave(ClientBtn);
+        }
+        private void ClientBtn_Click(object sender, EventArgs e)
+        {
+            buttonBackColor(ClientBtn);
+        }
+    }
+}
