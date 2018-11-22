78818889cce9012c4c429b2bc4d05f409ac6496a
Emil H <emilheu@libertymail.net>
2018-11-22 11:39:14 +0100

    Fix merge

---
+++ GUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unity;
+using BusinessLogic;
+using DataAccess;
+
+namespace GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (var container = new UnityContainer())
+            {
+                container.RegisterType<ICaseRepository, CaseRepository>();
+                Application.Run(container.Resolve<GUIForm>());
+            }
+        }
+    }
+}
--- GUI/Properties/AssemblyInfo.cs
+++ GUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Service")]
+[assembly: AssemblyTitle("GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Service")]
+[assembly: AssemblyProduct("GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("205aeedd-fa3b-46d4-be7d-486965b51d3d")]
+[assembly: Guid("4a9f7fb1-e6dd-4789-8f0a-153b862fd38d")]
 
 // Version information for an assembly consists of the following four values:
 //
