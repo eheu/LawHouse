c10b2dad7bcbd6121cbd3e9e306d3aa911079cee
Emil H <emilheu@libertymail.net>
2018-12-16 21:19:55 +0100

    Add Test project template

---
+++ BusinessLogic.Tests/AutoFakeItEasyDataAttribute.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using AutoFixture.Xunit2;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests
+{
+    public class AutoFakeItEasyDataAttribute : AutoDataAttribute
+    {
+        public AutoFakeItEasyDataAttribute()
+            : base(() => new Fixture().Customize(new AutoFakeItEasyCustomization()))
+        {
+        }
+    }
+}
